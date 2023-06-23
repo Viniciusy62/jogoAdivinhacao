c1895f88696f81a68d2a1df61e8c859be9f0eab6
Vinicius Andrade <viniciusy62@gmail.com>
2023-06-23 18:11:11 -0300

    create function random number

---
+++ main.js
@@ -0,0 +1,12 @@
+let btnVerify = document.querySelector("#btnVerify");
+
+function randomNumber() {
+  let random = 10 * Math.random();
+  let numberValid = Math.round(random) ;
+  return numberValid
+}
+
+// function verify(num) {
+//   num = document.querySelector("#number");
+
+// }
