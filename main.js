b76e00daf78d69b8019128cf8bea7d822930c717
Vinicius Andrade <viniciusy62@gmail.com>
2023-06-26 19:17:14 -0300

    update on some elements

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-let btnVerify = document.querySelector("#btnVerify");
+let btnVerify = document.querySelector("#btn-verify");
 
 function randomNumber() {
   let random = 10 * Math.random();
-  let numberValid = Math.round(random) ;
-  return numberValid
+  let numberValid = Math.round(random);
+  if(numberValid < 0) {
+    numberValid = 0;
+  }
+  return numberValid;
 }
 
-// function verify(num) {
-//   num = document.querySelector("#number");
+function comparateNumber(num) {
+  num = document.querySelector("#number");
+  let numberValid = randomNumber();
+  
+  console.log(numberValid);
 
-// }
+  if(numberValid == num.value) {
+    console.log("Acertou")
+  } else {
+    console.log("Errou!")
+  }
+  num.value = ""
+}
+
+btnVerify.addEventListener("click", comparateNumber)
