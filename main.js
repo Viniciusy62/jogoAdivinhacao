e17fd764a3b0154f7fb83c747509fc39c024703a
Vinicius Andrade <viniciusy62@gmail.com>
2023-06-27 14:27:02 -0300

    created function to change screen

--- main.js
+++ main.js
@@ -3,24 +3,32 @@ let btnVerify = document.querySelector("#btn-verify");
 function randomNumber() {
   let random = 10 * Math.random();
   let numberValid = Math.round(random);
-  if(numberValid < 0) {
+  if (numberValid < 0) {
     numberValid = 0;
   }
   return numberValid;
 }
 
+function changeScreen() {
+  let screen01 = document.querySelector(".screen01");
+  let screen02 = document.querySelector(".screen02");
+  
+  screen01.classList.toggle("hide");
+  screen02.classList.toggle("hide");
+}
+
 function comparateNumber(num) {
   num = document.querySelector("#number");
   let numberValid = randomNumber();
-  
+
   console.log(numberValid);
 
-  if(numberValid == num.value) {
-    console.log("Acertou")
+  if (numberValid == num.value) {
+    console.log("Acertou!");
   } else {
-    console.log("Errou!")
+    console.log("Errou!");
   }
-  num.value = ""
+  num.value = "";
 }
 
-btnVerify.addEventListener("click", comparateNumber)
+btnVerify.addEventListener("click", comparateNumber);
