f05594f034d157734a8042a466c64e7f2eb40c9e
Vinicius Andrade <viniciusy62@gmail.com>
2023-06-27 18:38:11 -0300

    update functions, let to const

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let btnVerify = document.querySelector("#btn-verify");
-let btnAgain = document.querySelector("#btn-again");
+const btnVerify = document.querySelector("#btn-verify");
+const btnAgain = document.querySelector("#btn-again");
 
 function randomNumber() {
-  let random = 10 * Math.random();
-  let numberValid = Math.round(random);
+  const random = 10 * Math.random();
+  const numberValid = Math.round(random);
   if (numberValid < 0) {
     numberValid = 0;
   }
@@ -11,8 +11,8 @@ function randomNumber() {
 }
 
 function changeScreen() {
-  let screen01 = document.querySelector(".screen01");
-  let screen02 = document.querySelector(".screen02");
+  const screen01 = document.querySelector(".screen01");
+  const screen02 = document.querySelector(".screen02");
   
   screen01.classList.toggle("hide");
   screen02.classList.toggle("hide");
@@ -20,9 +20,9 @@ function changeScreen() {
 
 function comparateNumber(num) {
   num = document.querySelector("#number");
-  let numberValid = randomNumber();
-  let txtValidate = document.querySelector("#txt-validate");
-  let result = document.querySelector("#result");
+  const numberValid = randomNumber();
+  const txtValidate = document.querySelector("#txt-validate");
+  const result = document.querySelector("#result");
 
   console.log(numberValid);
 
